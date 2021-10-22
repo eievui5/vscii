06207edf0bb2316abe722427acc5951a3d33aaaf
Eievui <14899090+GreenAndEievui@users.noreply.github.com>
2021-10-22 09:45:32 -0400

    Added error test.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def container_test():
     framebuffer = vscii.FrameBuffer()
 
     vlist = vscii.HSplit()
-    
+
     console0 = vscii.TextDisplay()
     vlist.add_child(console0)
 
@@ -21,7 +21,7 @@ def container_test():
 
     center = vscii.Center()
     center.add_child(hlist)
-    
+
     fullscreen = vscii.FullScreen()
     fullscreen.add_child(center)
     framebuffer.add_elem(fullscreen)
@@ -54,19 +54,42 @@ def tree_test():
     framebuffer.add_elem(nodes["fullscreen"])
 
     nodes["console0"].print("Hello World!")
-    nodes["console2"].print("L\n o\n  w\n   e\n    r")
-    nodes["console2"].print("world?")
+    nodes["console2"].print("Type something here: ")
     nodes["console1"].print("Goodbye World!")
     framebuffer.render()
-    nodes["console1"].print(
-        framebuffer.input(nodes["console1"].get_left() + 2,
-        nodes["console1"].get_bottom() - 2)
+    nodes["console2"].print(
+        framebuffer.input(nodes["console2"].get_left() + 2,
+        nodes["console2"].get_bottom() - 2) + "\nPress any key to continue."
     )
 
     framebuffer.getch()
 
+def error_test():
+    # Create a framebuffer and tree of elements.
+    nodes = vscii.read_tree({
+        "fullscreen": (vscii.FullScreen(), {
+            # This will raise a type error because integers are not framebuffer
+            # elements.
+            "text_display": 0,
+        }),
+    })
+
+    nodes["text_display"].print("Hello World!")
+
 def __main__():
-    tree_test()
+    try:
+        tree_test()
+        print("\033[32mPASS: Test case 1 did not raise an exception.")
+    except:
+        print("\033[31mFAIL: Test case 1 raised an exception.")
+
+    try:
+        error_test()
+        print("\033[31mFAIL: Test case 2 did not raise an exception.")
+    except:
+        print("\033[32mPASS: Test case 2 raised an exception.")
+
+    print("\033[0mFinished running test cases.")
 
 if __name__ == "__main__":
     __main__()
